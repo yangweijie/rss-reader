733cb714c93905b8f7c3a9ec90b70af5f867928d
yangweijie <917647288@qq.com>
2026-03-22 22:48:34 +0800

    删除

--- resources/js/bootstrap.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-window.axios = axios;
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-const token = document.head.querySelector('meta[name="csrf-token"]');
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-}
